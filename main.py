19f41fe786f6d6ac31701a52e982687e733fcaa6
unhingedNerd <nuho.hasan10@gmail.com>
2026-05-08 10:58:05 +0300

    First mmit

--- main.py
+++ main.py
@@ -2,11 +2,36 @@ import questionary
 import json
 import os
 
+
 TASK_PATH = os.path.join("data", "tasks.json")
 def view():
     with open(TASK_PATH, 'r') as file:
-        print(file.read())
+        data = json.load(file)
+        print(preetify(data))
 
+def preetify(content):
+    preety_content = ""
+    for taskName, taskData in content.items():
+        preety_content += f"""
+        task name: {taskName}
+        task completed: {taskData["complete"]}
+        task hidden: {taskData["hide"]}
+        """
+    return preety_content
+
+def add():
+    print("""
+    ===== Enter a new task =====""")
+    new_task = input("Task: ")
+    with open(TASK_PATH, 'r') as file:
+        data = json.load(file)
+    data[new_task] = {
+        "complete": False,
+        "hide": False
+    }
+    with open(TASK_PATH, 'w') as file:
+        json.dump(data, file, indent=4)
+    print(f"""==== Added task: {new_task}! ====""")
 
 def main():
     print("START")
@@ -16,9 +41,9 @@ def main():
     ).ask()
     match action:
         case "view":
-            print("VIEW FUNCTION WILL BE RAN HERE")
+            view()
         case "add":
-            print("ADD FUNCTION WILL BE ADDED HERE")
+            add()
 
 if __name__ == "__main__":
     main()
