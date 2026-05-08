4e6388078fe4c563eedaabb00fd6edf0cfceba43
unhingedNerd <nuho.hasan10@gmail.com>
2026-05-08 13:49:28 +0300

    Added toggle feature

--- main.py
+++ main.py
@@ -4,20 +4,47 @@ import os
 
 
 TASK_PATH = os.path.join("data", "tasks.json")
+SPACE_MIN = 2
 def view():
+    print("""
+    ===== View tasks =====""")
     with open(TASK_PATH, 'r') as file:
         data = json.load(file)
-        print(preetify(data))
+        choices = [
+            questionary.Choice(title=item, value=i)
+            for i, item in enumerate(preetify(data))
+        ]
+        task = questionary.select(
+            "Select to toggle",
+            choices=choices
+        ).ask()
+        if not(task == """ >> Back to main menu"""):
+            toggle(task, data)
 
 def preetify(content):
-    preety_content = ""
+    preety_array = []
+    longest = len(max(content.keys(), key=len)) + SPACE_MIN
     for taskName, taskData in content.items():
-        preety_content += f"""
-        task name: {taskName}
-        task completed: {taskData["complete"]}
-        task hidden: {taskData["hide"]}
+        differnce = longest - len(taskName)
+        spacing = ""
+        for i in range(1, differnce):
+            spacing += " "
+        preety_content = f"""{taskName}{spacing}| {"done" if taskData["complete"] else "not done"}
         """
-    return preety_content
+        preety_array.append(preety_content)
+
+    preety_array.append(""" >> Back to main menu""")
+    return preety_array
+
+def toggle(task, data):
+    items= list(data.items())
+    key, value = items[task]
+    value["complete"] = not(value["complete"])
+    value["hide"] = False
+    data = dict(items) 
+    with open(TASK_PATH, 'w') as file:
+        json.dump(data, file, indent=4)
+
 
 def add():
     print("""
@@ -31,7 +58,8 @@ def add():
     }
     with open(TASK_PATH, 'w') as file:
         json.dump(data, file, indent=4)
-    print(f"""==== Added task: {new_task}! ====""")
+        
+    print(f"""==== Added task: {new_task} ! ====""")
 
 def main():
     print("START")
